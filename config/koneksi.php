f9ba965b1403a45a92e6af50acd39aa917484039
kkurachanz <mitadwiiuu@gmail.com>
2025-12-24 20:08:23 +0800

    fix db connection

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,8 +2,8 @@
 	error_reporting(E_ERROR || E_PARSE);
 	date_default_timezone_set("Asia/Kuala_Lumpur");
 	$servername="localhost";
-	$username="root";
-	$password="";
+	$username="adminmjs";
+	$password="12345";
 
 	$conn=mysqli_connect($servername,$username,$password); 
 ?>
