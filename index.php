bd4f9db9ee18ba6b14824e0efc3c28e95abb635f
Elika <rey048286@gmail.com>
2025-12-24 17:10:20 +0800

    Add Index File

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laundry Maru</title>
+    <?php 
+        echo "<script type='text/javascript'>\n";
+        echo "window.location = ('login.php');";
+        echo "</script>"; 
+    ?>
+</head>
+<body>  
+</body>
+</html>
